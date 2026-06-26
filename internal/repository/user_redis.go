7166abcdeceb50d0f2f95f2665b2d82c0cb8e4b8
Ali506108 <aliduisen77@gmail.com>
2026-06-26 15:57:19 +0500

    feature/issue -0

--- internal/repository/user_redis.go
+++ internal/repository/user_redis.go
@@ -1,7 +1,38 @@
 package repository
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/Ali506108/UserProfile-Service/internal/domain"
+	"github.com/redis/go-redis/v9"
+)
 
 type userRedisRepo struct {
 	client *redis.Client
+}
+
+func (r *userRedisRepo) Save(ctx context.Context, user *domain.User) error {
+	data, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	return r.client.Set(ctx, user.ID, data, 0).Err()
+
+}
+
+func (r *userRedisRepo) GetById(ctx context.Context, id string) (*domain.User, error) {
+	data, err := r.client.Get(ctx, id).Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var user domain.User
+	if err := json.Unmarshal([]byte(data), &user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
